da6670a3e29d0928d67b57ac4ee46c99f8afcda1
HARI <rechealmatsikoIT@gmail.com>
2018-09-27 19:25:12 +0300

    pytest modified

--- tests/test_api.py
+++ tests/test_api.py
@@ -3,10 +3,10 @@ from flash.api import app
 from flask import json, request
 
 
-class Testing(unittest.TestCase):
+class Testentries(unittest.TestCase):
 
     def setUp(self):
-        self.client = app.test_client
+        self.client = app.test_client()
         self.entry = {
             'id': 0,
             'name': 'Racheal',
@@ -17,7 +17,7 @@ class Testing(unittest.TestCase):
         }
 
     def test_all_entries(self):
-        result = self.client().get('/api/v2/resources/entries/all')
+        result = self.client.get('/api/v2/resources/entries/all')
         self.assertEqual(result.status_code, 200)
 
     def test_single_entry(self):
@@ -30,15 +30,15 @@ class Testing(unittest.TestCase):
             'entry_added': 'Read upon my death, or just before yours'
         }
 
-        result = self.client().post('/api/v2/resources/entry/',
+        result = self.client.post('/api/v2/resources/entry/',
                                     content_type='application/json',
                                     data=json.dumps(entry)
                                     )
-       
+
         self.assertEqual(result.status_code, 201)
 
     def test_unavailable_fetch(self):
-        result = self.client().get('/api/v2/resources/entries/')
+        result = self.client.get('/api/v2/resources/entries/')
         self.assertEqual(result.status_code, 404)
 
     def test_entry_posted_successfully(self):
@@ -51,10 +51,10 @@ class Testing(unittest.TestCase):
             'entry_added': 'Read upon my death, or just before yours'
         }
 
-        result = self.client().post('/api/v2/resources/entry/',
-                                    content_type='application/json',
-                                    data=json.dumps(entry)
-                                    )
+        result = self.client.post('/api/v2/resources/entry/',
+                                  content_type='application/json',
+                                  data=json.dumps(entry)
+                                  )
 
         self.assertEqual(result.status_code, 201)
         self.assertIsNotNone(result)
@@ -68,7 +68,7 @@ class Testing(unittest.TestCase):
             'entry_added': 'Read upon my death, or just before yours'
         }
 
-        result = self.client().put('/api/v2/resources/entry/2',
+        result = self.client.put('/api/v2/resources/entry/2',
                                    content_type='application/json',
                                    data=json.dumps(entry)
                                    )
@@ -87,14 +87,14 @@ class Testing(unittest.TestCase):
             'entry_added': 'Read upon my death, or just before yours'
         }
 
-        result = self.client().post('/api/v2/resources/entry/',
+        result = self.client.post('/api/v2/resources/entry/',
                                     content_type='application/json',
                                     data=json.dumps(entry)
                                     )
-        
+
         entry_list.append(entry)
         self.assertEqual(result.status_code, 201)
-        
+
         update = {
             'id': 1,
             'name': 'Racheal',
@@ -106,7 +106,7 @@ class Testing(unittest.TestCase):
         entry = [entry for entry in entry_list]
         entry[0]['title'] = update['title']
         dict_title = {'title': entry[0]['title']}
-        result = self.client().put('/api/v2/resources/entry/2', content_type='application/json',
+        result = self.client.put('/api/v2/resources/entry/2', content_type='application/json',
                                    data=json.dumps(dict_title))
 
         self.assertEqual(result.status_code, 400)
@@ -114,23 +114,17 @@ class Testing(unittest.TestCase):
     def test_entry_deleted(self):
         delete_list = []
         delete = {
-                 'id': 1,
-                 'name': 'Racheal',
-                 'date': 10/6/2018,
-                 'title': 'My quite thoughts',
-                 'country': 'uganda',
-                 'entry_added': 'Read upon my death, or just before yours'
-             }
-        result = self.client().delete('/api/v2/resources/entry/',
-                                    content_type='application/json',
-                                    data=json.dumps(delete)
-                                    )
-       
+            'id': 1,
+            'name': 'Racheal',
+            'date': 10/6/2018,
+            'title': 'My quite thoughts',
+            'country': 'uganda',
+            'entry_added': 'Read upon my death, or just before yours'
+        }
+        result = self.client.delete('/api/v2/resources/entry/',
+                                      content_type='application/json',
+                                      data=json.dumps(delete)
+                                      )
+
         delete_list.append(delete)
         self.assertEqual(result.status_code, 405)
-        
-            
-        
-                
-
-
